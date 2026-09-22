73cdad70f8ef47279417f292b2166dac13126fc4
MaxymFediw <362706764@gotvdsb.ca>
2026-09-22 12:26:12 -0400

    Checkpoint 1

--- Lesson 4.5-Methods/Program.cs
+++ Lesson 4.5-Methods/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lesson_4._5_Methods
@@ -10,6 +11,11 @@ namespace Lesson_4._5_Methods
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press ENTER For A Joke.");
+
+            Console.ReadLine();
+
+            Joke(50);
 
 
 
@@ -20,7 +26,17 @@ namespace Lesson_4._5_Methods
 
 
 
+        }
 
+        public static void Joke(int numBugs) 
+        {
+            Console.WriteLine(numBugs + " little bugs in the code");
+            Thread.Sleep(500);
+            Console.WriteLine(numBugs + " little bugs.");
+            Thread.Sleep(500);
+            Console.WriteLine("Fix a bug, run it again,");
+            Thread.Sleep(500);
+            Console.WriteLine((numBugs++) + " little bugs in the code.");
 
 
         }
