8e61554e292f9cd73a43e1100ebaf70d125600ef
MaxymFediw <362706764@gotvdsb.ca>
2026-09-22 12:44:10 -0400

    All Done!

--- Lesson 4.5-Methods/Program.cs
+++ Lesson 4.5-Methods/Program.cs
@@ -11,11 +11,26 @@ namespace Lesson_4._5_Methods
     {
         static void Main(string[] args)
         {
+            int bugs;
+
+            Console.WriteLine("How many bugs do you want? (A full number, please.)");
+
+            if (Int32.TryParse(Console.ReadLine(), out bugs))
+            {
+                //int number = Gen.Next(bugs);
+            }
+
+            else 
+            {
+                Console.WriteLine("I guess, bro. You're gonna get 8 bugs, big guy.");
+                bugs = 8;
+            }
+
             Console.WriteLine("Press ENTER For A Joke.");
 
             Console.ReadLine();
 
-            Joke(50);
+            Joke(bugs);
 
 
 
@@ -36,7 +51,7 @@ namespace Lesson_4._5_Methods
             Thread.Sleep(500);
             Console.WriteLine("Fix a bug, run it again,");
             Thread.Sleep(500);
-            Console.WriteLine((numBugs++) + " little bugs in the code.");
+            Console.WriteLine((numBugs - 1) + " little bugs in the code.");
 
 
         }
